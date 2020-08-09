0128f0fc8687cc8652fed0918d48e4c2c3ae9cb8
GolferChen <671608295@qq.com>
2020-08-10 00:13:08 +0800

    2020-08-10

--- hasPathSum.py
+++ hasPathSum.py
@@ -8,13 +8,13 @@ class TreeNode:
         self.right = None
 
 # Recursive Version
-class Solution:
-    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
-        if root is None:
-            return False
-        if root.left is None and root.right is None:
-            return root.val == sum
-        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+# class Solution:
+#     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+#         if root is None:
+#             return False
+#         if root.left is None and root.right is None:
+#             return root.val == sum
+#         return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
 
 # Iterative Version, DFS
 # class Solution:
